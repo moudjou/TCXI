d18edd02b85f7cff0c749dd87a71d15e2f70a183
moudjou <na_mouda@esi.dz>
2024-12-21 15:28:17 +0100

    holiday page 100%

--- src/pages/employee/Holidays.jsx
+++ src/pages/employee/Holidays.jsx
@@ -1,11 +1,15 @@
 import Header from "../../components/Header"
 import Sidebar from "../../components/Sidebar"
+import VacationForm from "../../components/HolidayForm";
 export default function Holidays() {
   return (
-    <div className="flex">
+    <div className="flex ">
       <Sidebar />
+      
       <div className="flex flex-col w-screen">
         <Header />
+        <VacationForm className="w-full min-h-screen"/>
+
       </div>
     </div>
   );
